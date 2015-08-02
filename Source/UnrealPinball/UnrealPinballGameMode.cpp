258fcd788760fb61681513bf7cb786e9da99350e
TheMaster99 <wizzkid_99@yahoo.com>
2015-08-01 22:32:17 -0400

    First Commit

---
+++ Source/UnrealPinball/UnrealPinballGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealPinball.h"
+#include "UnrealPinballGameMode.h"
+
+
+
+
---
+++ Source/UnrealPinball/UnrealPinballGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UnrealPinballGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPINBALL_API AUnrealPinballGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
