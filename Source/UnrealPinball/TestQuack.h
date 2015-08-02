519cfaca2a3802e36e79578245dada3381e41cd5
VektorKnight <derekdelapeza@gmail.com>
2015-08-01 23:06:37 -0400

    Test
    Quack

---
+++ Source/UnrealPinball/TestQuack.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class UNREALPINBALL_API TestQuack
+{
+public:
+	TestQuack();
+	~TestQuack();
+};
